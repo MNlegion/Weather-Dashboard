71a689895421951cfe354e07a53d3497b9d20030
MNlegion <rkreuser30@gmail.com>
2022-04-15 22:28:30 -0500

    struggling to get the api fetch function working properly

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,58 +1,58 @@
 // var APIkey = "7366c7ee907740999d48cfcb373e945c"
-// var urlStart = "https://api.openweathermap.org/data/2.5/"
+// var urlStart = "https://api.openweathermap.org/data/2.5/onecall?"
 
-var formEl = document.querySelector("#city-search");
-var tasksToDoEl = document.querySelector("#search-history");
+// var formEl = document.querySelector("#city-search");
+// var tasksToDoEl = document.querySelector("#search-history");
 
-var taskFormHandler = function (event) {
-    event.preventDefault();
-    var taskNameInput = document.querySelector("input[name='search-input'").value;
+// var taskFormHandler = function (event) {
+//     event.preventDefault();
+//     var taskNameInput = document.querySelector("input[name='search-input'").value;
 
     // check if inputs are empty (validate)
-    if (taskNameInput === "") {
-        alert("You need to enter a location!");
-        return false;
-    }
+    // if (taskNameInput === "") {
+    //     alert("You need to enter a location!");
+    //     return false;
+    // }
 
     // reset form fields for next task to be entered
-    formEl.reset();
+//     formEl.reset();
 
-    document.querySelector("input[name='search-input']").value = "";
+//     document.querySelector("input[name='search-input']").value = "";
 
-    var taskDataObj = {
-        name: taskNameInput,
+//     var taskDataObj = {
+//         name: taskNameInput,
 
-    };
+//     };
 
-    createTaskEl(taskDataObj);
-};
+//     createTaskEl(taskDataObj);
+// };
 
-var createTaskEl = function (taskDataObj) {
+// var createTaskEl = function (taskDataObj) {
     // create list item
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "list-group";
+    // var listItemEl = document.createElement("li");
+    // listItemEl.className = "list-group";
 
     // create div to hold task info and add to list item
-    var taskInfoEl = document.createElement("div");
-    taskInfoEl.className = "list-group";
-    taskInfoEl.textContent = taskDataObj.name;
-    listItemEl.appendChild(taskInfoEl);
+    // var taskInfoEl = document.createElement("div");
+    // taskInfoEl.className = "list-group";
+    // taskInfoEl.textContent = taskDataObj.name;
+    // listItemEl.appendChild(taskInfoEl);
 
-    console.dir(listItemEl);
+    // console.dir(listItemEl);
 
     // add list item to list
-    tasksToDoEl.appendChild(listItemEl);
-};
+//     tasksToDoEl.appendChild(listItemEl);
+// };
 
-var saveSearch = function () {
-    localStorage.setItem("", JSON.stringify());
-}
+// var saveSearch = function () {
+//     localStorage.setItem("", JSON.stringify());
+// }
 
-formEl.addEventListener("submit", taskFormHandler);
+// formEl.addEventListener("submit", taskFormHandler);
 
-var getWeather = function () {
-    fetch("https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude=minutely,hourly&appid=7366c7ee907740999d48cfcb373e945c");
-}
+// var getWeather = function () {
+//     fetch("https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude=minutely,hourly&appid=7366c7ee907740999d48cfcb373e945c");
+// }
 
 
 
