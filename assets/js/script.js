1ce491534b8af94fa060ba24def225238505594e
MNlegion <rkreuser30@gmail.com>
2022-04-17 20:17:23 -0500

    reset index file and began to rescript script file, after much research I may have the idea on this

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,58 +1,33 @@
 // var APIkey = "7366c7ee907740999d48cfcb373e945c"
 // var urlStart = "https://api.openweathermap.org/data/2.5/onecall?"
 
-// var formEl = document.querySelector("#city-search");
-// var tasksToDoEl = document.querySelector("#search-history");
+var citySearchEl = document.getElementById("city-search");
+var cityFormEl = document.getElementById("city-form");
 
-// var taskFormHandler = function (event) {
-//     event.preventDefault();
-//     var taskNameInput = document.querySelector("input[name='search-input'").value;
+var APIkey = "7dd3d9ee3fd3a5e33bd10913e8e1557b";
+var cities = []
 
-    // check if inputs are empty (validate)
-    // if (taskNameInput === "") {
-    //     alert("You need to enter a location!");
-    //     return false;
-    // }
+var loadCities = function() {
+    var cityStorage = localStorage.getItem("cities")
+    if(!cityStorage) {
+        return false;
+    }
+    
+    cityStorage = JSON.parse(cityStorage);
+    
+    for (var i=0; i < cityStorage.length; i++) {
+        displaySearchedCities(cityStorage[i])
+        cities.push(cityStorage[i])
+    }
+}
 
-    // reset form fields for next task to be entered
-//     formEl.reset();
+var saveCities = function() {
+    localStorage.setItem("cities", JSON.stringify(cities));
+}
 
-//     document.querySelector("input[name='search-input']").value = "";
 
-//     var taskDataObj = {
-//         name: taskNameInput,
 
-//     };
 
-//     createTaskEl(taskDataObj);
-// };
-
-// var createTaskEl = function (taskDataObj) {
-    // create list item
-    // var listItemEl = document.createElement("li");
-    // listItemEl.className = "list-group";
-
-    // create div to hold task info and add to list item
-    // var taskInfoEl = document.createElement("div");
-    // taskInfoEl.className = "list-group";
-    // taskInfoEl.textContent = taskDataObj.name;
-    // listItemEl.appendChild(taskInfoEl);
-
-    // console.dir(listItemEl);
-
-    // add list item to list
-//     tasksToDoEl.appendChild(listItemEl);
-// };
-
-// var saveSearch = function () {
-//     localStorage.setItem("", JSON.stringify());
-// }
-
-// formEl.addEventListener("submit", taskFormHandler);
-
-// var getWeather = function () {
-//     fetch("https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude=minutely,hourly&appid=7366c7ee907740999d48cfcb373e945c");
-// }
 
 
 
